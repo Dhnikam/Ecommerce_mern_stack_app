ac38e22a25d4baa0f206ed512e3b31b7ea23b784
Dhnikam <122685654+Dhnikam@users.noreply.github.com>
2023-10-08 00:22:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,44 +2,49 @@ import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import { fileURLToPath } from 'url'; // Import fileURLToPath from url module
+import { dirname } from 'path'; // Import dirname from path module
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from 'path'
-//configure env
+import path from 'path';
+
+// Get the current module file path
+const __filename = fileURLToPath(import.meta.url);
+// Get the directory name of the current module file
+const __dirname = dirname(__filename);
+
+// Configure env
 dotenv.config();
 
-//databse config
+// Database config
 connectDB();
 
-//rest object
+// Rest object
 const app = express();
 
-//middelwares
+// Middlewares
 app.use(cors());
 app.use(express.json());
-app.use(morgan("dev")); 
-app.use(express.static(path.join(__dirname,'./client/build')))
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, './client/build')));
 
-//routes
+// Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-//rest api
-app.use('*',function(req,res){ 
-    res.sendFile(path.join(__dirname,"./client/build/index.html"));
+// Rest API
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-//PORT
+// PORT
 const PORT = process.env.PORT || 8080;
 
-//run listen
+// Run server
 app.listen(PORT, () => {
-  console.log(
-    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
-      .white
-  );
+  console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
 });
